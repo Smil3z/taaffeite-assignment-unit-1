25eed87a9f7c0fc55fb4109cd152e6e26f49306d
Stephon Tonge <stephon.tonge75@gmail.com>
2023-08-16 11:48:44 -0500

    index.html

---
+++ assignment/helloWorld.js
@@ -0,0 +1,31 @@
+console.log('hello world!');
+
+//variables
+//let used to declare variable
+//firstName is the variable  and 'Chris' is the value were assigning
+
+let firstName = 'Stephon';
+console.log(firstName);
+
+let lastName = 'Tonge';
+console.log(lastName);
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+//variable set to the number 25
+let age = 25;
+console.log(age);
+
+//boolean variable type
+let loggedIn = true;
+//console log with the string and value
+console.log('loggedIn' , loggedIn);
+
+let isRaining = false;
+
+// conditional statement
+if (loggedIn) {
+    // if (true) will run this code
+    console.log('welcome', fullName);
+}
